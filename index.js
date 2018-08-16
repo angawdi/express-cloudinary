e878cf3a5693f31ef174b87db0951f0ba70b272a
Sky <anvandp18@gmail.com>
2018-08-16 14:42:16 -0700

    just test the .env file

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+var cloudinary = require('cloudinary');
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
 var app = express();
+var upload = multer({ dest: './uploads/'});
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post('/', upload.single('myFile'), function(req, res) {
+	console.log('req.file:', req.file);
 });
 
 app.listen(3000);
